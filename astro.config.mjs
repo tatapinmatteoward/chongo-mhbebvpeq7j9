9974c973014e1d8198bddb77eb8dd8144504fcaa
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 02:49:36 +0000

    Add astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'static',
+  site: 'https://chongo-mhbebvpeq7j9.pages.dev'
+});
